151f3a89d13bd0a6c770871ebbf68b616ceb36fd
Allen Sanders <a_sanders_2003@msn.com>
2016-05-09 22:46:24 -0400

    Initial commit

---
+++ Kata04/Kata04/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Kata 04!!");
+            Console.ReadLine();
+        }
+    }
+}
