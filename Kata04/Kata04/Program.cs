fdb0da2d1cbf31f7e5852b180bb74f26eb5c79b9
Allen Sanders <a_sanders_2003@msn.com>
2016-05-10 09:56:44 -0400

    Part 1 of kata complete

--- Kata04/Kata04/Program.cs
+++ Kata04/Kata04/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +11,41 @@ namespace Kata04
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Kata 04!!");
-            Console.ReadLine();
+            string dataPath = null;
+            List<string[]> inputData = new List<string[]>();
+
+            Console.WriteLine("Let's MUNGE some data!!");
+            Console.WriteLine("****************************************************************");
+
+            try
+            {
+                dataPath = GetPathToFile();
+                inputData = WeatherProcessor.ParseDataFile(dataPath);
+                Console.WriteLine(WeatherProcessor.EvaluateData(inputData));
+            }
+            catch (Exception e)
+            {
+                // TODO: Log something "tech-y" here to help IT troubleshoot root cause
+                Console.WriteLine("Error encountered while trying to process input data - please contact IT for assistance");
+            }
+        }
+
+        private static string GetPathToFile()
+        {
+            string dataPath = null;
+            bool inputIsInvalid = false;
+
+            do
+            {
+                Console.Write("Please provide full path to source data: ");
+                dataPath = Console.ReadLine();
+                if (inputIsInvalid = !File.Exists(dataPath))
+                {
+                    Console.WriteLine("Please specify a valid path!");
+                }
+            } while (inputIsInvalid);
+
+            return dataPath;
         }
     }
 }
---
+++ Kata04/Kata04/WeatherProcessor.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata04
+{
+    public class WeatherProcessor
+    {
+        public static List<string[]> ParseDataFile(string dataPath)
+        {
+            List<string[]> inputData = new List<string[]>();
+
+            using (StreamReader reader = new StreamReader(dataPath))
+            {
+                string inputLine = null;
+                while ((inputLine = reader.ReadLine()) != null)
+                {
+                    if (!inputLine.Trim().Equals(string.Empty))
+                    {
+                        string[] rawDataRow = inputLine.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                        // TODO: Evaluate assumptions about structure of data - what if first three data values not always populated?
+                        inputData.Add(rawDataRow.Take(3).ToArray());
+                    }
+                }
+            }
+
+            return inputData;
+        }
+
+        public static string EvaluateData(List<string[]> inputData)
+        {
+            int minDayNumber = int.MinValue;
+            double minSpread = double.MaxValue;
+
+            foreach (var dataRow in inputData)
+            {
+                int dayNumber = 0;
+                double maxTemp = 0.0;
+                double minTemp = 0.0;
+                double rowSpread = double.MaxValue;
+                if (int.TryParse(dataRow[0], out dayNumber))
+                {
+                    // TODO: Evaluate assumptions about presence of * at end of highest/lowest temps
+                    dataRow[1] = dataRow[1].TrimEnd('*');
+                    dataRow[2] = dataRow[2].TrimEnd('*');
+                    if (double.TryParse(dataRow[1], out maxTemp) && double.TryParse(dataRow[2], out minTemp))
+                    {
+                        rowSpread = maxTemp - minTemp;
+                        if (rowSpread < minSpread)
+                        {
+                            minDayNumber = dayNumber;
+                            minSpread = rowSpread;
+                        }
+                    }
+                }
+            }
+
+            return $"Day #{minDayNumber} had the smallest temperature spread at {minSpread:F2}";
+        }
+    }
+}
